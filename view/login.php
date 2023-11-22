d100eb27c86c1b05db03a6586b6f0ba1b86bd365
pablitoodt <pablodeteba@icloud.com>
2023-11-22 10:42:47 +0100

    page login.php

---
+++ view/login.php
@@ -0,0 +1,10 @@
+<div>
+    <h3>Sign in</h3>
+    <form method="post">
+        <label for="email" >Email</label>
+        <input type="email" name="email" ><br>
+        <label for="password">Password</label>
+        <input type="password" name="password"><br>
+        <input type="submit" value="valider" />
+    </form>
+</div>
