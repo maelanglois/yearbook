f938f9398738af04b62935b582adb645af060d18
neyzar <lucas.narguet451@gmail.com>
2023-11-22 10:36:19 +0100

    creation d'un fichier connection.php pour pouvoir ce connecter a la base de donnée

---
+++ root/connection.php
@@ -0,0 +1,18 @@
+<?php
+
+# CONNECTION BASE DE DONNÉE MAC
+$bdd = new PDO('mysql:host=localhost;dbname=yearbook;charset=utf8;port:8889','root', 'root');
+
+$bdd->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+$bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+# CONNECTION BASE DE DONNÉE WINDOWS
+
+/*
+{
+    $bdd = new PDO('mysql:host=localhost;dbname=yearbook;charset=utf8', 'root', '');
+    $bdd->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+    $bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+}
+*/
