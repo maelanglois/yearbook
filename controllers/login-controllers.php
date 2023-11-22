d95bc36cac8bed928d858ebe54623ba70d22adae
pablitoodt <pablodeteba@icloud.com>
2023-11-22 11:54:27 +0100

    ajout du login-controllers.php

---
+++ controllers/login-controllers.php
@@ -0,0 +1,24 @@
+<?php
+
+require './root/connection.php';
+
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $query = "SELECT * FROM students WHERE email = :email";
+    $stmt = $bdd->prepare($query);
+    $stmt->bindParam(':email', $_POST['email']);
+    $stmt->execute();
+    
+    $user = $stmt->fetch();
+
+    if ($user && password_verify($_POST['password'], $user['password'])) {
+        $_SESSION['status'] = 'connected';
+        $_SESSION['id'] = $user['id'];
+        $_SESSION['name'] = $user['name'];
+        $_SESSION['firstname'] = $user['firstname'];
+        header("Location: ./view/homepage.php");
+        exit();
+    } else {
+        echo "Invalid email or password.";
+    }
+}
