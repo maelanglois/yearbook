6e0d06fbfae6f4d2a81ce9d80c40d769fc01de2a
mohnsun <manonlanglois.mala@gmail.com>
2023-11-23 11:05:17 +0100

    détails homepage

--- view/homepage.php
+++ view/homepage.php
@@ -14,15 +14,15 @@
         <div class="homepage-year-display">
             <div class="homepage-year-contain">
                 <div class="homepage-grayscale"><img src="images/01.jpg" class="homepage-year-img"></div>
-                <div class="homepage-year-title">année 2022 - 2023</div>
+                <a href="#" title=" année XXXX" class="homepage-year-title">année 2022 - 2023</a>
                 <div class="homepage-year-number">XX étudiants</div>
             </div>
             <div class="homepage-year-contain">
                 <div class="homepage-grayscale"><img src="images/01.jpg" class="homepage-year-img"></div>
-                <div class="homepage-year-title">année 2022 - 2023</div>
+                <a href="" title="année XXX" class="homepage-year-title">année 2022 - 2023</a>
                 <div class="homepage-year-number">XX étudiants</div>
             </div>
-            
+            <a href="#" class="homepage-add-align" title="Ajouter un yearbook"><ion-icon name="add-circle-outline" class="homepage-add"></ion-icon></a>
         </div>
     </div>
 </body>
