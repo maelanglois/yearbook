3e916a31e45601cde996acf034258b15cca6bf4d
sano <fawez.azzouk@coda-student.school>
2023-11-23 14:41:16 +0100

    Suppression de l'HTML <<inutile>> dans le delete-yearbook.php

--- view/delete-yearbook.php
+++ view/delete-yearbook.php
@@ -15,15 +15,3 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     } 
 }
 ?>
-
-<!DOCTYPE html>
-<html lang="fr">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Delete a yearbook</title>
-</head>
-<body>
-    <h2>Suppression d'un yearbook</h2>
-</body>
-</html>
