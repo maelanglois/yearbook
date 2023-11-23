e42de0ede15130226de33d763c7afadea6348da3
mohnsun <manonlanglois.mala@gmail.com>
2023-11-23 09:59:36 +0100

    register + modifs login et layout (lien icon)

--- view/login.php
+++ view/login.php
@@ -2,12 +2,14 @@
 <div class="container">
     <div class="login-card">
         <div class="login-title">Login</div>
-        <form method="post" class="login-display">
-            <div class="login-int-display"><label for="email" class="login-label">Email</label>
-            <input type="email" name="email" class="login-input"></div>
-            <div class="login-int-display"><label for="password" class="login-label">Password</label>
-            <input type="password" name="password" class="login-input"></div>
-            <input type="submit" value="valider" class="login-submit" />
+        <form method="post">
+            <div class="login-display">
+                <div class="login-int-display"><label for="email" class="login-label">Email</label>
+                <input type="email" name="email" class="login-input"></div>
+                <div class="login-int-display"><label for="password" class="login-label">Password</label>
+                <input type="password" name="password" class="login-input"></div>
+                <input type="submit" value="Valider" class="login-submit" />
+            </div>
         </form>
     </div>
 </div>
--- view/register.php
+++ view/register.php
@@ -1,35 +1,22 @@
-<?php 
-require './root/connection.php'
 
-?>
-
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-<div>
-    <h1>inscription eleve</h1>
-
-    <form action="/controllers/register-controller.php" method="post" enctype="multipart/form-data">
-        <label for="name">name</label>
-        <input type="text" placeholder="enter name" name="name"><br><br>
-        <label for="firstname">firstname</label>
-        <input type="text" placeholder="enter lastname" name="lastname"><br><br>
-        <label for="email">email</label>
-        <input type="email" name="email" placeholder="enter email"><br><br>
-        <label for="password">password</label>
-        <input type="password" name="password" placeholder="enter password"><br><br>
-        <label for="avatar">photo de l'eleve</label>
-        <input type="file" name="avatar" id="">
-        <input type="submit">
-
-    </form>
+<div class= container>
+    <div class="login-card">
+        <div class="login-title">Inscription élève</div>
+            <form action="/controllers/register-controller.php" method="post" enctype="multipart/form-data">
+                <div class="login-display">
+                    <div class="login-int-display"><label for="name" class="login-label">Name</label>
+                    <input type="text" placeholder="Enter name" name="name" class="login-input"></div>
+                    <div class="login-int-display"><label for="firstname" class="login-label">Last name</label>
+                    <input type="text" placeholder="Enter lastname" name="lastname" class="login-input"></div>
+                    <div class="login-int-display"><label for="email" class="login-label">Email</label>
+                    <input type="email" name="email" placeholder="Enter email" class="login-input"></div>
+                    <div class="login-int-display"><label for="password" class="login-label">Password</label>
+                    <input type="password" name="password" placeholder="Enter password" class="login-input"></div>
+                    <div class="login-int-display"><label for="avatar" class="login-label">Photo de profil</label>
+                    <input type="file" name="avatar" id="" class="login-input"></div>
+                    <input type="submit" class="login-submit" value ="S'enregistrer">
+                </div>
+            </form>
+        </div>
+    </div>
 </div>
-    
-</body>
-</html>
