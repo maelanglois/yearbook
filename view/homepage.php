771c4cfd04af5d75aee79e8153ddde1fcc6c4033
mohnsun <manonlanglois.mala@gmail.com>
2023-11-23 15:37:47 +0100

    ajout darkmode css (pas encore bouton changement)

--- view/homepage.php
+++ view/homepage.php
@@ -4,11 +4,16 @@
     <div class="homepage-year-display">
         <?php 
         foreach($datas as $data) {?>
+        <div>
         <a href="#"><div class="homepage-year-contain">
             <div class="homepage-grayscale"><img src="images/<?= $data['image']?>" class="homepage-year-img"></div>
                 <div class="homepage-year-title"><?= $data['start_period']?> - <?= $data['end_period']?></div>
             <div class="homepage-year-number">XX étudiants</div>
         </div></a>
+        <div class="yearbook-modiadd">
+            <a href="" class="yearbook-delete"><ion-icon name="trash-outline" class="yearbook-mod-icon"></ion-icon>Supprimer
+        </div></a>
+        </div>
         <?php } ?>
     </div>
 </div>
