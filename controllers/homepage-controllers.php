8c009cc481257cf4ffa8106683fda1e736d39dbd
pablitoodt <pablodeteba@icloud.com>
2023-11-23 12:23:16 +0100

    automatisation de l'ajout des promotions

--- controllers/homepage-controllers.php
+++ controllers/homepage-controllers.php
@@ -1,3 +1,10 @@
 <?php
 
+require './root/connection.php';
+ 
+$query='SELECT * FROM promotions';
+
+$response = $bdd->query($query);
+$datas = $response->fetchAll();
+
 require dirname(__DIR__) . '/view/homepage.php';
