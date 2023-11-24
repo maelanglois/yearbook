fae5e0d0b3bc81a03c17d0a0f339da00dc2cc668
pablitoodt <pablodeteba@icloud.com>
2023-11-24 14:35:21 +0100

    ajout de la suppresion d'un yearbook

--- controllers/delate-yearbook-controllers.php
+++ controllers/delate-yearbook-controllers.php
@@ -1,20 +1,17 @@
 <?php
 require dirname(__DIR__) . '/root/connection.php';
-
+require dirname(__DIR__) . '/view/delate-yearbook.php';
 
 if($_SERVER['REQUEST_METHOD'] === 'POST'){
-    $id=$_POST['id'];
+    $id=$_GET['userId'];
     var_dump($id);
     $_SESSION['sup']= $id;
     var_dump($_SESSION['sup']);
-    if ($_POST['confirmation'] === 'OUI'){
-        $req = $bdd->prepare('DELETE FROM promotions WHERE id = :id');
-        $req->execute(array(
-            ':id'=> $_SESSION['sup']
-            ));
-            //header('location: ?page=homepage');
-    }
+    $req = $bdd->prepare('DELETE s.*, p.* FROM students as s INNER JOIN promotions AS p ON s.promotion_id=s.id WHERE s.promotion_id=:id and p.id=:id;');
+    $req->execute(array(':id'=> $id));
+    header('location: ?page=homepage');
 
 }
 
-require dirname(__DIR__) . '/view/delate-yearbook.php';
+
+
--- view/delate-yearbook.php
+++ view/delate-yearbook.php
@@ -1,7 +1,7 @@
 <div class= container>
     <div class="login-card">
         <div class="login-title">Voulez vous bien supprimer cette promotion</div>
-            <form action="" method="post" enctype="multipart/form-data">
+            <form action="" method="POST" enctype="multipart/form-data">
                 <div class="login-display">
                     <div class="login-int-display">
                         <input type="text" placeholder="Enter OUI ou NON" name="confirmation" class="login-input">
