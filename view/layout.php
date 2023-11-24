c2802b62ed1526ecc80559342d3fccb5baa98f67
mohnsun <manonlanglois.mala@gmail.com>
2023-11-24 14:10:27 +0100

    light/nightmode

--- view/layout.php
+++ view/layout.php
@@ -8,7 +8,7 @@
     <link href="https://fonts.googleapis.com/css2?family=Inter&display=swap" rel="stylesheet">
     <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
     <script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
-    <link rel="stylesheet" href="css/darkstyle.css">
+    <?php if(isset($_POST["nightmode"])){ ?><link rel="stylesheet" href="css/style.css"><?php } else{?><link rel="stylesheet" href="css/darkstyle.css"><?php } ?>
     <title><?= $route ?></title>
 </head>
 <body>
@@ -20,7 +20,13 @@
                 <div class="nav">
                     <a href="?page=homepage" title="homepage" class="nav-link">home</a>
                     <span class="link-display">
-                        <ion-icon name="sunny-outline" class="nav-icon"></ion-icon>
+                        <form method="POST">
+                        <?php if(isset($_POST["nightmode"])){ ?>
+                            <button type="submit" class="nav-icon" name="lightmode"><ion-icon name="moon-outline"></ion-icon></button>
+                        <?php } else {?> 
+                            <button type="submit" class="nav-icon" name="nightmode"><ion-icon name="sunny-outline"></ion-icon></button><?php 
+                        }?>
+                        </form>
                         <a href="?page=logout" title="logout" class="nav-link">logout</a> 
                     </span>
                 </div>  
