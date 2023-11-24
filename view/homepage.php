35eb852849d60cb2f78a97a5a69b5078a06f942d
pablitoodt <pablodeteba@icloud.com>
2023-11-24 12:06:08 +0100

    homepage

--- view/homepage.php
+++ view/homepage.php
@@ -11,8 +11,11 @@
             <div class="homepage-year-number">XX étudiants</div>
         </div></a>
         <div class="yearbook-modiadd">
-            <a href="?page=delate-yearbook" class="yearbook-delete"><ion-icon name="trash-outline" class="yearbook-mod-icon"></ion-icon>Supprimer
-        </div></a>
+            <form method="POST" action="?page=delate-yearbook">
+                <input name="id" value="<?= $data['id']?>"hidden></input>
+                <button type="submit" name="submit" class="yearbook-delete"><ion-icon name="trash-outline" class="yearbook-mod-icon"></ion-icon>Supprimer</button>
+            </form>
+        </div>
         </div>
         <?php } ?>
     </div>
