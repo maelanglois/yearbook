613209b2a777d4a66d3b9fb2e152cc5b8a5f969d
neyzar <lucas.narguet451@gmail.com>
2023-11-24 10:38:38 +0100

    enfin le register marche

--- controllers/register-controllers.php
+++ controllers/register-controllers.php
@@ -1,15 +1,39 @@
 <?php 
-require './root/connection.php';
-require dirname(__DIR__) . '/view/register.php';
 
-if(isset($_POST["title"]) && isset($_POST["text"]) && isset($_POST["date"]) && isset($_FILES['avatar']) && $_FILES['avatar']['error'] === 0 && $_FILES['avatar']['size'] <= 2000000 && $_FILES['avatar']['type'] === 'avatar/png')
-{
+require './root/connection.php';
+include './view/register.php';
+echo '<pre>';
+print_r($_POST);
+echo '</pre>';
+var_dump($_FILES);
+
+$requete = $bdd->query("select * from students");
+
+
+$sql = "INSERT INTO students (name, firstname, email, password, birthday, avatar, promotion_id, role, aime) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)";
+$requete = $bdd->prepare($sql);
+ echo $requete->execute(array($_POST['name'], $_POST['firstname'], $_POST['email'], $_POST['password'],$_POST['birthday'], 'avatar', $_POST['promotion_id'],$_POST['role'], $_POST['aime']));
+echo $bdd->lastInsertId();
+
+
+if(isset($_POST["name"]) && isset($_POST["firstname"]) && isset($_POST["email"]) && isset($_POST["password"]) && isset($_POST["promotion"]) && isset($_POST["birthday"]) && isset($_FILES['avatar']) && $_FILES['avatar']['error'] === 0 && $_FILES['avatar']['size'] <= 200000 && $_FILES['avatar']['type'] === 'avatar/png'){
+    
+   
+    $avatarPath = './login/' . basename($_FILES['avatar']['name']);
     $password = password_hash($_POST['password'], PASSWORD_DEFAULT );   
     $sql = "INSERT INTO students (name, firstname, email, password, birthday, avatar, promotion) VALUES (?, ?, ?, ?, ?, ?, ?)";
     $requete = $bdd->prepare($sql);
 
-    move_uploaded_file($_FILES['avatar']['tmp_name'], '/login' . basename($_FILES['avatar']['name']));
-    $requete->execute(array($_POST['name'], $_POST['firstname'], $_POST['email'], $password, $_POST['birthday'], $_POST['avatar'], $_POST['promotion']));
+    move_uploaded_file($_FILES['avatar']['tmp_name'], './login/' . basename($_FILES['avatar']['name']));
+    try {
+        $requete->execute(array($_POST['name'], $_POST['firstname'], $_POST['email'], $password, $_POST['birthday'], $avatarPath, $_POST['promotion']));
+        echo $bdd->lastInsertId();
+    } catch(exception $e)
+    {
+        echo $e->getMessage();    
+    }
+ 
+    
 
     
 }
--- view/register.php
+++ view/register.php
@@ -2,21 +2,28 @@
 <div class= container>
     <div class="login-card">
         <div class="login-title">Inscription élève</div>
-            <form action="/controllers/register-controller.php" method="post" enctype="multipart/form-data">
+            <form action="" method="post" enctype="multipart/form-data">
                 <div class="login-display">
                     <div class="login-int-display"><label for="name" class="login-label">Name</label>
-                    <input type="text" placeholder="Enter name" name="name" class="login-input"></div>
-                    <div class="login-int-display"><label for="firstname" class="login-label">Last name</label>
-                    <input type="text" placeholder="Enter firstname" name="firstname" class="login-input"></div>
+                    <input type="text" placeholder="Enter name" name="firstname" class="login-input"></div>
+                    <div class="login-int-display"><label for="firstname" class="login-label">first name</label>
+                    <input type="text" placeholder="Enter lastname" name="name" class="login-input"></div>
                     <div class="login-int-display"><label for="email" class="login-label">Email</label>
                     <input type="email" name="email" placeholder="Enter email" class="login-input"></div>
                     <div class="login-int-display"><label for="password" class="login-label">Password</label>
                     <input type="password" name="password" placeholder="Enter password" class="login-input"></div>
-                    <div class="login-int-display"><label for="avatar" class="login-label">Photo de profil</label>
+                    <label for="date">anniversaire</label>
+                    <input type="date" name="birthday" id="">
+                    <label for="promotion">promo</label>
+                    <input type="number" name="promotion_id" id="">
+                    <label for="role">role</label>
+                    <input type="text" name="role">
+                    <label for="aime">aime</label>
+                    <input type="text" name="aime" value='0'>
+                     <div class="login-int-display"><label for="avatar" class="login-label">Photo de profil</label>
                     <input type="file" name="avatar" id="" class="login-input"></div>
                     <input type="submit" class="login-submit" value ="S'enregistrer">
                 </div>
             </form>
         </div>
     </div>
-</div>
